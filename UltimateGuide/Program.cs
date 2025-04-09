f19410324a91fc25bb069a671037743cff139fd7
Chitrang <chitrang313@gmail.com>
2025-04-09 22:42:18 +0530

    18 Http Status Code

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) => { 
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync("Hello!!!");
+    await context.Response.WriteAsync(" World");
+});
 
-app.Run();
+app.Run(); 
