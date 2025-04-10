2c71683fd49d333ad67e6c0a78619a3285fdf8d2
Chitrang <chitrang313@gmail.com>
2025-04-10 06:06:00 +0530

    21 Query String

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -2,11 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (HttpContext context) => {
-    string path = context.Request.Path;
-    string method = context.Request.Method;
-    context.Response.ContentType = "text/html";
-    await context.Response.WriteAsync($"<p>{path}</p>");
-    await context.Response.WriteAsync($"<p>{method}</p>");
+	context.Response.ContentType = "text/html";
+	if (context.Request.Method == "GET") {
+		string key = "id";
+		if (context.Request.Query.ContainsKey(key)) {
+			string? value = context.Request.Query[key];
+			await context.Response.WriteAsync($"<p>{value}</p>");
+		}
+    }
 });
 
 app.Run();
