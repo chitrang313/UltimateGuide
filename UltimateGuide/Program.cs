42bb5b34266545c5a51d2478fe55b50a0608e8e7
Chitrang <chitrang313@gmail.com>
2025-04-10 07:38:10 +0530

    25 HTTP Get vs Post

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -1,12 +1,22 @@
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (HttpContext context) => {
-    context.Response.ContentType = "text/html";
-    string key = "AuthorizationKey";
-    if (context.Request.Headers.ContainsKey(key)) {
-        string? value = context.Request.Headers[key];
-        await context.Response.WriteAsync($"<p>{value}</p>");
+    StreamReader reader = new StreamReader(context.Request.Body);
+    string result = await reader.ReadToEndAsync();
+
+    Dictionary<string,StringValues> dictionary = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(result);
+
+    if (dictionary.ContainsKey("firstname")) {
+        StringValues firstName = dictionary["firstname"];
+        await context.Response.WriteAsync($"{firstName}");
+    }
+    if (dictionary.ContainsKey("age")) {
+        foreach (string? item in dictionary["age"]) {
+            await context.Response.WriteAsync($"\n{item}");
+        }
     }
 });
 
