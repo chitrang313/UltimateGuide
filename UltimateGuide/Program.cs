f47610efd24f3757d59bb9b201e4bd7238a08be9
Chitrang <chitrang313@gmail.com>
2025-04-10 06:15:52 +0530

    sample url added

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -3,7 +3,8 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) => {
 	context.Response.ContentType = "text/html";
-	if (context.Request.Method == "GET") {
+    //http://localhost:5000/?id=123&name=ram
+    if (context.Request.Method == "GET") {
 		string key = "id";
 		if (context.Request.Query.ContainsKey(key)) {
 			string? value = context.Request.Query[key];
