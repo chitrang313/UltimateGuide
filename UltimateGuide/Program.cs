296199a36594b639522f50ce17df09aad22dc77d
Chitrang <chitrang313@gmail.com>
2025-04-10 06:24:42 +0530

    22 HTTP Request Header

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -3,14 +3,8 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) => {
 	context.Response.ContentType = "text/html";
-    //http://localhost:5000/?id=123&name=ram
-    if (context.Request.Method == "GET") {
-		string key = "id";
-		if (context.Request.Query.ContainsKey(key)) {
-			string? value = context.Request.Query[key];
-			await context.Response.WriteAsync($"<p>{value}</p>");
-		}
-    }
+	string? userAgent = context.Request.Headers["User-Agent"];
+	await context.Response.WriteAsync($"<p>{userAgent}</p>");
 });
 
 app.Run();
