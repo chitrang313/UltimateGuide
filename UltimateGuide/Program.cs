e93782e7d528552f2abcd96babb8e1c6366f157b
Chitrang <chitrang313@gmail.com>
2025-04-10 05:44:18 +0530

    20 HTTP Request

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -2,12 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (HttpContext context) => {
-    context.Response.Headers["MyKey"] = "MyValue";
-    context.Response.Headers["Server"] = "Custom Server";
-    context.Response.Headers["Content-Type"] = "text/html";
-
-    await context.Response.WriteAsync("<h1>Hello!!!</h1>");
-    await context.Response.WriteAsync("<h2> World</h2>");
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync($"<p>{path}</p>");
+    await context.Response.WriteAsync($"<p>{method}</p>");
 });
 
-app.Run(); 
+app.Run();
