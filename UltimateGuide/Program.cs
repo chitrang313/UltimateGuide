506c15df05fa8db3ed05984b566188057d77a824
Chitrang <chitrang313@gmail.com>
2025-04-11 10:32:50 +0530

    38 Intro for Routing

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -1,19 +1,16 @@
-using Microsoft.Extensions.Primitives;
 using UltimateGuide.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<MyCustomMiddleware>();
 
 var app = builder.Build();
 
-app.UseWhen(context => context.Request.Query.ContainsKey("username"),
-    app => {
-        app.Use(async (context,next) => {
-            await context.Response.WriteAsync("\nUsername Found");
-            await next(context);
-        });
-    });
+//Enable Routing
+app.UseRouting();
 
-app.Run(context => context.Response.WriteAsync("Main Chain Middleware"));
+//creating end points
+app.UseEndpoints(endpoints => {
+
+});
+
 
 app.Run();
