ab1900b71ffbb7c76d778002cfb60977aeeaccb6
Chitrang <chitrang313@gmail.com>
2025-04-11 16:01:12 +0530

    52 Creating Controllers

--- UltimateGuide/Controllers/HomeController.cs
+++ UltimateGuide/Controllers/HomeController.cs
@@ -3,8 +3,20 @@
 namespace UltimateGuide.Controllers {
     public class HomeController:Controller {
         [Route("sayhello")]
+        [Route("/")]
         public string Index() {
             return "Hello from the Index of Home";
+        }
+
+        [Route("about")]
+        public string About() {
+            return "Hello from the About of Home";
+        }
+
+        [Route("contact-us")]
+        [Route("contact/{mobile:regex(^\\d{{10}}$)}")]
+        public string Contact() {
+            return "Hello from the Contact of Home";
         }
     }
 }
