657d867f19211ffe4747b0c5c81d7f8a46c8ed44
Chitrang <chitrang313@gmail.com>
2025-04-11 09:31:32 +0530

    31 Custom Middleware

---
+++ UltimateGuide/Middleware/MyCustomMiddleware.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UltimateGuide.Middleware {
+    public class MyCustomMiddleware:IMiddleware {
+        public async Task InvokeAsync(HttpContext context,RequestDelegate next) {
+            await context.Response.WriteAsync("\nCustom Middleware Start");
+            await next(context);
+            await context.Response.WriteAsync("\nCustom Middleware End");
+        }
+    }
+}
--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -1,19 +1,19 @@
 using Microsoft.Extensions.Primitives;
+using UltimateGuide.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
+
 var app = builder.Build();
 
 //middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) => {
-    await context.Response.WriteAsync("Middleware 1");
+    await context.Response.WriteAsync("\nMiddleware 1");
     await next(context);
 });
 
 //middleware 2
-app.Use(async (HttpContext context,RequestDelegate next) => {
-    await context.Response.WriteAsync("\nMiddleware 2");
-    await next(context);
-});
+app.UseMiddleware<MyCustomMiddleware>();
 
 //middleware 3
 app.Run(async (HttpContext context) => {
