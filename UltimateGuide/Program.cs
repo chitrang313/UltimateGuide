6cea0cbc256986cb6bcc0a064e242a71f8d75d9c
Chitrang <chitrang313@gmail.com>
2025-04-11 11:59:49 +0530

    42 Default Values in Route Parameters

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -15,10 +15,16 @@ app.UseEndpoints(endpoints => {
         await context.Response.WriteAsync($"Reading File: {filename} - {extension}");
     });
 
-    endpoints.Map("employees/profile/{name}", async (context) => {
+    endpoints.Map("employees/profile/{name=user}", async (context) => {
         string? name = context.Request.RouteValues["name"].ToString();
         await context.Response.WriteAsync($"Employee Profile: {name}");
     });
+
+    endpoints.Map("product/detail/{id=0}",async (context) => {
+        int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsync($"Product Detail: {id}");
+    });
+
 });
 
 app.Run(async (context) => { 
