5b5fc8c0c0f2e8edce22fc22b010a111489b112f
Chitrang <chitrang313@gmail.com>
2025-04-11 10:53:54 +0530

    39 Map, Map Get, Map Post

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -9,8 +9,17 @@ app.UseRouting();
 
 //creating end points
 app.UseEndpoints(endpoints => {
+    endpoints.MapGet("map1",async (context) => { 
+        await context.Response.WriteAsync("map1 execute");
+    });
 
+    endpoints.MapPost("map2",async (context) => {
+        await context.Response.WriteAsync("map2 execute");
+    });
 });
 
+app.Run(async (context) => { 
+    await context.Response.WriteAsync($"Request received at: {context.Request.Path}");
+});
 
 app.Run();
