be2d58a3ff12283b4c31c907a7729e1eb8891f9c
Chitrang <chitrang313@gmail.com>
2025-04-11 14:37:01 +0530

    49 Web root path

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -1,56 +1,19 @@
 using UltimateGuide.Middleware;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions() { 
+    WebRootPath = "myroot"
+});
 
 var app = builder.Build();
 
-//Enable Routing
+app.UseStaticFiles();
+
 app.UseRouting();
 
-//creating end points
 app.UseEndpoints(endpoints => {
-    endpoints.Map("files/{filename}.{extension}",async (context) => {
-        string? filename = Convert.ToString(context.Request.RouteValues["filename"]);
-        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
-        await context.Response.WriteAsync($"Reading File: {filename} - {extension}");
+    endpoints.Map("/",async context => {
+        await context.Response.WriteAsync("Hello");
     });
-
-    endpoints.Map("employees/profile/{name?}",async (context) => {
-        if (context.Request.RouteValues.ContainsKey("name")) {
-            string? name = context.Request.RouteValues["name"].ToString();
-            await context.Response.WriteAsync($"Employee Profile: {name}");
-        } else {
-            await context.Response.WriteAsync($"Employee Profile: No Name provided");
-        }
-    });
-
-    endpoints.Map("product/detail/{id?}",async (context) => {
-        if (context.Request.RouteValues.ContainsKey("id")) {
-            int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
-            await context.Response.WriteAsync($"Product Detail: {id}");
-        } else {
-            await context.Response.WriteAsync($"Product Detail: No ID provided");
-        }
-    });
-
-    endpoints.Map("daily-digest-report/{reportdate:datetime?}",async (context) => {
-        if (context.Request.RouteValues.ContainsKey("reportdate")) {
-            DateTime selectedDate = Convert.ToDateTime(context.Request.RouteValues["reportdate"]);
-            await context.Response.WriteAsync($"Report: {selectedDate.ToShortDateString()}");
-        } else {
-            await context.Response.WriteAsync($"Report: No Date provided");
-        }
-    });
-
-    endpoints.Map("cities/{cityid:guid}",async (context) => {
-        Guid cityId = Guid.Parse((string)context.Request.RouteValues["cityid"]);
-        await context.Response.WriteAsync($"City ID: {cityId}");
-    });
-
-});
-
-app.Run(async (context) => {
-    await context.Response.WriteAsync($"Request received at: {context.Request.Path}");
 });
 
 app.Run();
