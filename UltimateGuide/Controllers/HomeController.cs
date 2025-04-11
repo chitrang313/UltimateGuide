c99a72d4fcb074b35df78868ea1e46d326648832
Chitrang <chitrang313@gmail.com>
2025-04-11 18:05:07 +0530

    58 IActionResult

--- UltimateGuide/Controllers/HomeController.cs
+++ UltimateGuide/Controllers/HomeController.cs
@@ -3,55 +3,30 @@ using UltimateGuide.Models;
 
 namespace UltimateGuide.Controllers {
     public class HomeController:Controller {
-        [Route("sayhello")]
-        [Route("/")]
-        public ContentResult Index() {
-            //return new ContentResult() {
-            //    ContentType = "text/plain",
-            //    Content = "Hello from the Home Controller",
-            //};
+        [Route("file")]
+        [HttpPost]
+        public IActionResult Index() {
+            if (!Request.Query.ContainsKey("bookid")) {
+                Response.StatusCode = 400;
+                return Content("Book ID not provided");
+            }
 
-            //return Content("Hello from the Home Controller","text/plain");
+            if (string.IsNullOrWhiteSpace((string)Request.Query["bookid"])) {
+                Response.StatusCode = 400;
+                return Content("Book ID is empty");
+            }
 
-            return Content("<h1>Hello</h1><h3>from the html</h3>", "text/html");
+            int bookId = Convert.ToInt32(Request.Query["bookid"]);
+            if (bookId <= 0 || bookId > 1000) {
+                return Content("Book ID must be greter then zero and less then 1000");
+            }
+            bool isLoggedIn = Convert.ToBoolean(Request.Query["isloggedin"]);
+            if (!isLoggedIn) {
+                Response.StatusCode = 401;
+                return Content("You must be logged in to view this page");
+            }
+            return File("sample.pdf","application/pdf");
         }
 
-        [Route("person")]
-        public JsonResult Person() {
-            Person person = new Person() {
-                Id = Guid.NewGuid(),
-                firstName = "username",
-                lastName = "surname",
-                age = 30,
-            };
-            //return new JsonResult(person);
-            return Json(person);
-        }
-
-        [Route("file-download")]
-        public VirtualFileResult DownloadFile() {
-            //return new VirtualFileResult("/sample.pdf","application/pdf");
-            return File("/sample.pdf","application/pdf");
-        }
-        
-        //D:\Web\Placeholders
-        [Route("file-download-2")]
-        public PhysicalFileResult DownloadFilePhysically() {
-            //return new PhysicalFileResult(@"D:/Web/Placeholders/sample.pdf","application/pdf");
-            return PhysicalFile(@"D:/Web/Placeholders/sample.pdf","application/pdf");
-        }
-
-        [Route("file-download-3")]
-        public FileContentResult DownloadFileContent() {
-            byte[] bytes = System.IO.File.ReadAllBytes(@"D:/Web/Placeholders/sample.pdf");
-            //return new FileContentResult(bytes,"application/pdf");
-            return File(bytes,"application/pdf");
-        }
-
-        [Route("contact-us")]
-        [Route("contact/{mobile:regex(^\\d{{10}}$)}")]
-        public string Contact() {
-            return "Hello from the Contact of Home";
-        }
     }
 }
