9f607c5a03b792297989f933950c5184a82dd956
Chitrang <chitrang313@gmail.com>
2025-04-12 15:57:39 +0530

    72 All Possible model validation rules

--- UltimateGuide/Models/Person.cs
+++ UltimateGuide/Models/Person.cs
@@ -11,12 +11,26 @@ namespace UltimateGuide.Models {
         public string? firstName { get; set; }
         public string? lastName { get; set; }
         public int age { get; set; }
+
+        [EmailAddress(ErrorMessage = "{0} need to be in a prper format")]
+        [Required(ErrorMessage = "{0} can't be blank")]
         public string email { get; set; }
+
+        [Phone(ErrorMessage = "{0} must contain 10 Digit only")]
+        public string phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
         public string? password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("password",ErrorMessage = "{0} and {1} does not matched")]
+        [Display(Name = "Confirm Password")]
         public string? confirmPassword { get; set; }
+
 
         [Range(0,999.99,ErrorMessage = "{0} should be in between {1}$ to {2}$")]
         public double price { get; set; }
+
         public override string ToString() {
             return $"{Id}, {firstName}, {lastName}, {age}, {email}, {password}, {confirmPassword}";
         }
