dcf56759509993d43a6e4e3354cf95f4553e14a8
Chitrang <chitrang313@gmail.com>
2025-04-12 15:01:48 +0530

    69 Model Validation

--- UltimateGuide/Controllers/HomeController.cs
+++ UltimateGuide/Controllers/HomeController.cs
@@ -5,47 +5,15 @@ using UltimateGuide.Models;
 
 namespace UltimateGuide.Controllers {
     public class HomeController:Controller {
-        [Route("bookstore/{bookid?}/{isloggedin?}")]
-        public IActionResult Index(int? bookid, bool? isloggedin, Book book) {
-            if (!bookid.HasValue) {
-                //Response.StatusCode = 400;
-                //return Content("Book ID is empty");
-                return BadRequest("Book ID is not supplied or empty");
-            }
 
-            if (bookid <= 0 || bookid > 1000) {
-                //return Content("Book ID must be greter then zero and less then 1000");
-                return NotFound("Book ID must be greater than zero and less than 1000");
-            }
+        [Route("/")]
+        public IActionResult Index() {
+            return Content("<h1>Home Page</h1>","text/html");
+        }
 
-            if (isloggedin.HasValue && !Convert.ToBoolean(isloggedin)) {
-                //Response.StatusCode = 401;
-                //return Content("You must be logged in to view this page");
-                return Unauthorized("You must be logged in to view this page");
-            }
-            //return File("sample.pdf","application/pdf");
-
-            //302 - Temporary Redirect
-            //return new RedirectToActionResult("Books", "Store", new { });
-            //return RedirectToAction("Books","Store",new { id = bookid });
-
-            //301 - Permanent Redirect
-            //return new RedirectToActionResult("Books", "Store", new { },permanent: true);
-            //return RedirectToActionPermanent("Books","Store",new { id = bookid });
-
-            //302 - Temporary Redirect
-            //LocalRedirectResult we are using for locally redirect using url
-            //return new LocalRedirectResult($"store/books/{bookid}");
-            //return LocalRedirect($"store/books/{bookid}");
-
-            //301 - Permanent Redirect
-            //return new LocalRedirectResult($"store/books/{bookid}",permanent: true);
-            //return LocalRedirectPermanent($"store/books/{bookid}");
-
-            //return Redirect($"store/books/{bookid}");
-            //return RedirectPermanent($"/store/books/{bookid}");
-
-            return Content($"<h1>{book.ToString()}</h1>","text/html");
+        [Route("register")]
+        public IActionResult Register(Person person) {
+            return Content($"{person}","text/plain");
         }
     }
 }
--- UltimateGuide/Models/Person.cs
+++ UltimateGuide/Models/Person.cs
@@ -1,8 +1,18 @@
-﻿namespace UltimateGuide.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UltimateGuide.Models {
     public class Person {
         public Guid Id{ get; set; }
+        [Required]
         public string? firstName { get; set; }
         public string? lastName { get; set; }
         public int age { get; set; }
+        public string email { get; set; }
+        public string? password { get; set; }
+        public string? confirmPassword { get; set; }
+
+        public override string ToString() {
+            return $"{Id}, {firstName}, {lastName}, {age}, {email}, {password}, {confirmPassword}";
+        }
     }
 }
