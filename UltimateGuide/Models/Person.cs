9e32e3c6b6eca917fdf64a7a2912f7cd9d42c9f8
Chitrang <chitrang313@gmail.com>
2025-04-12 15:37:59 +0530

    71 Model Validation part one

--- UltimateGuide/Models/Person.cs
+++ UltimateGuide/Models/Person.cs
@@ -1,9 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UltimateGuide.Models {
     public class Person {
         public Guid Id{ get; set; }
-        [Required(ErrorMessage = "Person name can't be null or empty")]
+
+        [Required(ErrorMessage = "{0} can't be null or empty")]
+        [Display(Name = "Person Name")]
+        [StringLength(maximumLength:40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} charactor long")]
         public string? firstName { get; set; }
         public string? lastName { get; set; }
         public int age { get; set; }
@@ -11,6 +15,8 @@ namespace UltimateGuide.Models {
         public string? password { get; set; }
         public string? confirmPassword { get; set; }
 
+        [Range(0,999.99,ErrorMessage = "{0} should be in between {1}$ to {2}$")]
+        public double price { get; set; }
         public override string ToString() {
             return $"{Id}, {firstName}, {lastName}, {age}, {email}, {password}, {confirmPassword}";
         }
