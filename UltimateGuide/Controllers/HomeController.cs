c813a82b09a3097c9b72964c68cdf7f0044d774c
Chitrang <chitrang313@gmail.com>
2025-04-13 18:11:10 +0530

    86 Views Initial Commit

---
+++ UltimateGuide/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltimateGuide.Controllers {
+    public class HomeController:Controller {
+        [Route("home")]
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
