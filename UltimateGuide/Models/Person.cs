c20d849a9251c60d764ffdad2fc916d43a619301
Chitrang <chitrang313@gmail.com>
2025-04-18 09:12:23 +0530

    89 Switch case

--- UltimateGuide/Models/Person.cs
+++ UltimateGuide/Models/Person.cs
@@ -2,5 +2,9 @@
     public class Person {
         public string? Name{ get; set; }
         public DateTime? DateofBirth { get; set; }
+        public Gender Gender { get; set; }
+    }
+    public enum Gender { 
+        Male, Female, Other
     }
 }
