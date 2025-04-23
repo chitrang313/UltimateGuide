28691b46c1c2135de157479d02d196c5cd72cb81
Chitrang <chitrang313@gmail.com>
2025-04-23 07:30:26 +0530

    105 Creating a Layout View part one

---
+++ UltimateGuide/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltimateGuide.Controllers {
+    public class HomeController:Controller {
+        [Route("/")]
+        public IActionResult Index() {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About() {
+            return View();
+        }
+
+        [Route("contact-us")]
+        public IActionResult Contact() {
+            return View();
+        }
+    }
+}
---
+++ UltimateGuide/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltimateGuide.Controllers {
+    public class ProductController:Controller {
+        [Route("products")]
+        public IActionResult Index() {
+            return View();
+        }
+
+        [Route("search")]
+        public IActionResult Search() {
+            return View();
+        }
+
+        [Route("order")]
+        public IActionResult Order() {
+            return View();
+        }
+    }
+}
