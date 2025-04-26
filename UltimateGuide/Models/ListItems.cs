fe50c3fe6d8718c90e775df2b78b0d80287e6fa9
Chitrang <chitrang313@gmail.com>
2025-04-26 15:19:34 +0530

    117 Strongly typed partial views

---
+++ UltimateGuide/Models/ListItems.cs
@@ -0,0 +1,6 @@
+﻿namespace UltimateGuide.Models {
+    public class ListItems {
+        public string ItemName { get; set; } = "";
+        public List<string> ItemsList { get; set; } = new List<string>();
+    }
+}
