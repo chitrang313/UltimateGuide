f98226508f711cefe55cf4a3b6fc51a1201e863f
Chitrang <chitrang313@gmail.com>
2025-04-27 08:43:02 +0530

    118 Partial ViewResult

--- UltimateGuide/Controllers/HomeController.cs
+++ UltimateGuide/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UltimateGuide.Models;
 
 namespace UltimateGuide.Controllers {
     public class HomeController:Controller {
@@ -10,6 +11,17 @@ namespace UltimateGuide.Controllers {
         [Route("about")]
         public IActionResult About() {
             return View();
+        }
+
+        [Route("programming-language")]
+        public IActionResult ProgramingLanguage() {
+            ListItems listItems = new ListItems();
+            listItems.ItemName = "Programming Language";
+            listItems.ItemsList.Add("C#");
+            listItems.ItemsList.Add("Java");
+            listItems.ItemsList.Add("Python");
+
+            return PartialView("_ListPartialView", listItems);
         }
     }
 }
