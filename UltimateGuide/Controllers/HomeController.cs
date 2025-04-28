56450af78e07a844522ead88040b0536a5ce1de6
Chitrang <chitrang313@gmail.com>
2025-04-28 22:03:31 +0530

    121 Creating View Components

---
+++ UltimateGuide/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltimateGuide.Controllers {
+    public class HomeController:Controller {
+        [Route("/")]
+        public IActionResult Index() {
+            return View();
+        }
+        [Route("/about")]
+        public IActionResult About() {
+            return View();
+        }
+    }
+}
---
+++ UltimateGuide/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltimateGuide.ViewComponents {
+    [ViewComponent]
+    public class GridViewComponent : ViewComponent{
+        Task<IViewComponentResult> InvokAsync() {
+            return View(); //invoke partial view path: "Views/Shared/Components/Grid/Default.cshtml"
+        }
+    }
+}
