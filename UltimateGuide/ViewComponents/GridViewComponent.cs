29897f751cd17ea0440ef51a90dea70de5e9c761
Chitrang <chitrang313@gmail.com>
2025-04-29 21:50:28 +0530

    125 View Componnts with Parameters

--- UltimateGuide/ViewComponents/GridViewComponent.cs
+++ UltimateGuide/ViewComponents/GridViewComponent.cs
@@ -3,21 +3,8 @@ using UltimateGuide.Models;
 
 namespace UltimateGuide.ViewComponents {
     public class GridViewComponent:ViewComponent {
-        public async Task<IViewComponentResult> InvokeAsync() {
-            PersonGridModel personGridModel = new PersonGridModel() {
-                GridTitle = "Persons List",
-                Persons = new List<Person>() {
-                new Person(){
-                    PersonName = "Chitrang",
-                    PersonJobTitle = "Software Engineer"
-                },
-                new Person(){
-                    PersonName = "User",
-                    PersonJobTitle = "Data Analitics Engineer"
-                },
-                }
-            };
-            return View("Sample",personGridModel);
+        public async Task<IViewComponentResult> InvokeAsync(PersonGridModel gridModel){
+            return View("Sample",gridModel);
         }
     }
 }
