043b71dc83eeacaf5082a4af7b1b0d5bd2e9729e
Chitrang <chitrang313@gmail.com>
2025-04-29 20:03:11 +0530

    122 Creating View Components

--- UltimateGuide/ViewComponents/GridViewComponent.cs
+++ UltimateGuide/ViewComponents/GridViewComponent.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace UltimateGuide.ViewComponents {
-    [ViewComponent]
     public class GridViewComponent : ViewComponent{
-        Task<IViewComponentResult> InvokAsync() {
-            return View(); //invoke partial view path: "Views/Shared/Components/Grid/Default.cshtml"
+        public async Task<IViewComponentResult> InvokeAsync() { 
+            return View();
         }
     }
 }
