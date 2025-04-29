a1cf59f60ad37ebe6f1f91d8abf9c24a85daa275
Chitrang <chitrang313@gmail.com>
2025-04-29 20:21:28 +0530

    122 Renamed possible for ViewComponent cshtml file Tag Helper can use to invoke ViewComponent

--- UltimateGuide/ViewComponents/GridViewComponent.cs
+++ UltimateGuide/ViewComponents/GridViewComponent.cs
@@ -3,7 +3,7 @@
 namespace UltimateGuide.ViewComponents {
     public class GridViewComponent : ViewComponent{
         public async Task<IViewComponentResult> InvokeAsync() { 
-            return View();
+            return View("Sample");
         }
     }
 }
