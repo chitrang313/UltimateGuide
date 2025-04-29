68291167254b83f77ab2365538ececdbd7561d2f
Chitrang <chitrang313@gmail.com>
2025-04-29 23:37:34 +0530

    126 View component result

--- UltimateGuide/Controllers/HomeController.cs
+++ UltimateGuide/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UltimateGuide.Models;
 
 namespace UltimateGuide.Controllers {
     public class HomeController:Controller {
@@ -9,6 +10,24 @@ namespace UltimateGuide.Controllers {
         [Route("/about")]
         public IActionResult About() {
             return View();
+        }
+
+        [Route("friends-list")]
+        public IActionResult FriendList() {
+            PersonGridModel friendsGridModel = new PersonGridModel() {
+                GridTitle = "Friends List",
+                Persons = new List<Person>() {
+                new Person(){
+                    PersonName = "friend one",
+                    PersonJobTitle = "Devops Engineer"
+                },
+                new Person(){
+                    PersonName = "friend two",
+                    PersonJobTitle = "AIML Engineer"
+                },
+                }
+            };
+            return ViewComponent("Grid",friendsGridModel);
         }
     }
 }
---
+++ UltimateGuide/wwwroot/friend.js
@@ -0,0 +1,6 @@
+﻿document.querySelector("#load-friends-button").addEventListener("click", async function () {
+  //example che k bank application ma statement load krvu hoi, on specific button nai k pehla page load thai tyare j krvanu hoi. to aa technique use thai che.
+  var response = await fetch("friends-list", { method: "GET" });
+  var responseBody = await response.text();
+  document.querySelector("#list").innerHTML = responseBody;
+});
