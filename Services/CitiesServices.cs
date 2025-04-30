1e533d06e58153ebcaa00799222ae7071dd9891f
Chitrang <chitrang313@gmail.com>
2025-04-30 08:39:20 +0530

    130 Services Usecase

---
+++ Services/CitiesServices.cs
@@ -0,0 +1,16 @@
+﻿namespace Services {
+    public class CitiesServices {
+        private List<string> _cityList;
+        public CitiesServices() {
+            _cityList = new List<string>() { 
+                "London",
+                "New York",
+                "Tokyo",
+                "Paris",
+            };
+        }
+        public List<string> GetCities() {
+            return _cityList;
+        }
+    }
+}
---
+++ UltimateGuide/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace UltimateGuide.Controllers {
+    public class HomeController:Controller {
+        private readonly CitiesServices _citiesServices;
+        public HomeController() {
+            _citiesServices = new CitiesServices();
+        }
+
+        [Route("/")]
+        public IActionResult Index() {
+            List<string> _cityList = _citiesServices.GetCities();
+            return View(_cityList);
+        }
+    }
+}
