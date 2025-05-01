bde6e58277a02a221642d3df66c863a18c1976bd
Chitrang <chitrang313@gmail.com>
2025-05-01 22:33:44 +0530

    134 Method Injection FromService

--- UltimateGuide/Controllers/HomeController.cs
+++ UltimateGuide/Controllers/HomeController.cs
@@ -4,14 +4,15 @@ using ServiceContracts;
 
 namespace UltimateGuide.Controllers {
     public class HomeController:Controller {
-        private readonly ICityServices _citiesServices;
+        /*private readonly ICityServices _citiesServices;
         public HomeController(ICityServices cityServices) {
             _citiesServices = cityServices;
         }
+        */
 
         [Route("/")]
-        public IActionResult Index() {
-            List<string> _cityList = _citiesServices.GetCities();
+        public IActionResult Index([FromServices]ICityServices _cityServices) {
+            List<string> _cityList = _cityServices.GetCities();
             return View(_cityList);
         }
     }
