0967e13d835364affe1670cb88854829edde7039
Chitrang <chitrang313@gmail.com>
2025-05-07 17:36:53 +0530

    138 Added short hand writen note

--- UltimateGuide/Program.cs
+++ UltimateGuide/Program.cs
@@ -4,11 +4,15 @@ using Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
-builder.Services.Add(new ServiceDescriptor(
-    typeof(ICityServices),
-    typeof(CitiesServices),
-    ServiceLifetime.Scoped
-    ));
+//builder.Services.Add(new ServiceDescriptor(
+//    typeof(ICityServices),
+//    typeof(CitiesServices),
+//    ServiceLifetime.Scoped
+//    ));
+
+builder.Services.AddTransient<ICityServices,CitiesServices>();
+builder.Services.AddScoped<ICityServices,CitiesServices>();
+builder.Services.AddSingleton<ICityServices,CitiesServices>();
 
 var app = builder.Build();
 
